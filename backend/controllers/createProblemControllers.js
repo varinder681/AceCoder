e3d68fb82c9db25e412c08ff706a29fcb3700f65
Varinder Singh <varindersingh6885@gmail.com>
2021-12-11 19:41:04 +0530

    minor updates

--- backend/controllers/createProblemControllers.js
+++ backend/controllers/createProblemControllers.js
@@ -43,6 +43,6 @@ export const submit = async (req, res) => {
         }
     } catch (error) {
         res.status(500)
-        res.json({error : 'Internal Server Error jhkj'})
+        res.json({error : 'Internal Server Error'})
     }
 }
